6e8411ac28d46a09bd1362806afc5b8a819ec17f
Nikhil Maurya <nikhilmaurya2601@gmail.com>
2023-07-11 13:39:06 +0530

    Addition file addded

---
+++ Addition.java
@@ -0,0 +1,7 @@
+class Addition{
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println("The sum of given number is : "+(a+b));
+    }
+}
---
+++ sum.java
@@ -0,0 +1,7 @@
+class sum{
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println("The sum of given number is : "+(a+b));
+    }
+}
